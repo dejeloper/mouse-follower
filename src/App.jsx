03b0200030157a890cd0cc8eee73fad4c70fd4d5
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-30 22:48:03 -0500

    Add mouse Follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
+import { useEffect, useState } from "react"
 import './App.css'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
 
   return (
-    <>
-      <h1>Mouse Follewer</h1>
-    </>
+    <main>
+      <h3 style={{ pointerEvents: "none" }}>Mouse Follower</h3>
+      <div style={{
+        position: "absolute",
+        backgroundColor: "#000000dc",
+        border: '1px solid #fff',
+        borderRadius: '100%',
+        opacity: 0.8,
+        pointerEvents: "none",
+        top: -25,
+        left: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }} />
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? "Desactivar" : "Activar"} seguir Puntero</button>
+    </main>
   )
 }
 
